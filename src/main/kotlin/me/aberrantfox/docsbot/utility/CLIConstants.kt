80c6360567f99421889bbbfa7056800738ee6d12
AberrantFox <aberrantfoxtph@gmail.com>
2018-12-15 00:51:25 +0000

    Clean up of constants and addition of CLI Constants

---
+++ src/main/kotlin/me/aberrantfox/docsbot/utility/CLIConstants.kt
@@ -0,0 +1,6 @@
+package me.aberrantfox.docsbot.utility
+
+object CLIConstants {
+    const val TokenMessage = "Your discord bot token"
+    const val RootDirectoryMessage = "Where the files will be stored (relative path)"
+}
--- src/main/kotlin/me/aberrantfox/docsbot/utility/FileConstants.kt
+++ src/main/kotlin/me/aberrantfox/docsbot/utility/FileConstants.kt
@@ -1,21 +1,8 @@
 package me.aberrantfox.docsbot.utility
 
-import java.io.File
 
 object FileConstants {
-    val Project_Directory: String = "docbot${File.separator}"
-    val Documentation_Directory: String = "$Project_Directory${File.separator}docs${File.separator}"
-    val Configuration_Directory: String = "$Project_Directory${File.separator}config${File.separator}"
-
-    val Project_Directory_File: File = File(Project_Directory)
-    val Documentation_Directory_File: File = File(Documentation_Directory)
-    val Configuration_Directory_File: File = File(Configuration_Directory)
-
-    init {
-        Project_Directory_File.mkdir()
-        Documentation_Directory_File.mkdir()
-        Configuration_Directory_File.mkdir()
-    }
-
-    fun dbFilePath(language: String) = "${FileConstants.Documentation_Directory}$language.json"
+    const val Project_Directory_Default: String = "docbot"
+    const val Documentation_Directory: String = "docs"
+    const val Configuration_Directory: String = "config"
 }
